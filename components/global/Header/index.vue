0bbff943e12897c955b71d7e84e00bce59abf752
Pat Williams <patrick.rw92@gmail.com>
2018-05-25 18:43:01 -0400

    nav

--- components/global/Header/index.vue
+++ components/global/Header/index.vue
@@ -5,11 +5,17 @@
         <img src="https://bulma.io/images/bulma-logo.png" alt="Bulma: a modern CSS framework based on Flexbox" width="112" height="28">
       </a>
 
-      <a role="button" class="navbar-burger is-active" aria-label="menu" aria-expanded="false">
+      <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false">
         <span aria-hidden="true"></span>
         <span aria-hidden="true"></span>
         <span aria-hidden="true"></span>
       </a>
+    </div>
+    <div class="navbar-menu">
+      <div class="navbar-end">
+        <nuxt-link class="navbar-item" to="/">Home</nuxt-link>
+        <nuxt-link class="navbar-item" to="/contact">Contact</nuxt-link>
+      </div>
     </div>
   </nav>
 </template>
