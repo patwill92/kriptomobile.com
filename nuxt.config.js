62ddb95b0a2ced9d83be40b98030e884d6d253e7
Pat Williams <patrick.rw92@gmail.com>
2018-06-03 22:02:40 -0400

    favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,6 +5,13 @@ module.exports = {
       {charset: 'utf-8'},
       {name: 'viewport', content: 'width=device-width, initial-scale=1'},
       {hid: 'description', name: 'description', content: 'Nuxt.js project'}
+    ],
+    link: [
+      {
+        rel: 'icon',
+        type: 'image/x-icon',
+        href: 'kriptomobile.ico',
+      }
     ]
   },
   css: [
