669401b5a1bbbcdd8b7d63b073d18939c143cb37
Pat Williams <patrick.rw92@gmail.com>
2018-06-06 19:18:10 -0400

    server fix

--- plugins/twitter.js
+++ plugins/twitter.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 import {GET_TWEETS} from "@/types";
 
 export default async ({store, isStatic, isDev}, inject) => {
-  const endPoint = isDev ? 'http://localhost:5000/api/twitter' : 'https://twitterkripto.herokuapp.com/api/twitter';
+  const endPoint = isDev ? 'http://localhost:4000/api/twitter' : 'https://twitterkripto.herokuapp.com/api/twitter';
   const res = await axios.get(endPoint);
   inject('axios', axios);
   store.commit(`twitter/${GET_TWEETS}`, res.data);
